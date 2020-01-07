97ee87263daaf286a3f77dc2b52a91a18a570166
Mauri Vinicius <maurimoura@ioasys.com.br>
2020-01-07 09:09:18 -0300

    TINDEV START

---
+++ TinDev/backend/src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+
+const server = express();
+mongoose.connect('mongodb+srv://mauri:M210818v@cluster0-hk10w.mongodb.net/omnistack8?retryWrites=true&w=majority',
+    { useNewUrlParser:true, useUnifiedTopology: true
+    });
+
+server.use(express.json());
+server.use(routes);
+server.listen(3333);
+
+
+// video da aula 2 aos 49:07
